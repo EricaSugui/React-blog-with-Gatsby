a3fbd52cdb1109ba5a4898f5a0b51f3cace3024a
Erica Suguimoto <erica@iMac-de-Erica.local>
2021-05-15 21:40:54 -0300

    finishing the course

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,7 +87,7 @@ module.exports = {
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-sitemap`, // gatsby build && gatsby serve
   ]
